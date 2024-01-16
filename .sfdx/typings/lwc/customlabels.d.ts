79769f9b54d56813017314982b5c5833f30ca3a3
Lakshmi Dharani Rengasamy <lakshmi.dharani@internetbrands.com>
2024-01-16 13:19:15 +0530

    Demo Devops #2

---
+++ .sfdx/typings/lwc/customlabels.d.ts
@@ -0,0 +1,32 @@
+declare module "@salesforce/label/c.Bond_Prod" {
+    var Bond_Prod: string;
+    export default Bond_Prod;
+}
+declare module "@salesforce/label/c.Bond_UAT" {
+    var Bond_UAT: string;
+    export default Bond_UAT;
+}
+declare module "@salesforce/label/c.DaySmart_10" {
+    var DaySmart_10: string;
+    export default DaySmart_10;
+}
+declare module "@salesforce/label/c.Demandforce_Privacy_Policy" {
+    var Demandforce_Privacy_Policy: string;
+    export default Demandforce_Privacy_Policy;
+}
+declare module "@salesforce/label/c.Opp_Owner" {
+    var Opp_Owner: string;
+    export default Opp_Owner;
+}
+declare module "@salesforce/label/c.PrivacyPolicy" {
+    var PrivacyPolicy: string;
+    export default PrivacyPolicy;
+}
+declare module "@salesforce/label/c.Role_Names" {
+    var Role_Names: string;
+    export default Role_Names;
+}
+declare module "@salesforce/label/c.TermsOfService_WebMD" {
+    var TermsOfService_WebMD: string;
+    export default TermsOfService_WebMD;
+}
